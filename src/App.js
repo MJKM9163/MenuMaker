beb0551d4d97873bd17c1b41bc045c6b9a7c9069
rhkrrbaud <rhkrrbaudgg@naver.com>
2021-12-21 06:58:18 +0900

    진짜 최종 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MakerLoading from './containers/makers/MakerLoading';
 import MenuBar from './components/MenuBar';
 import SettingContainer from './containers/setting/SettingContainer';
 import ComentContainer from './containers/coments/ComentContainer';
-import DBmakerForm from './containers/DBmakerForm';
+//import DBmakerForm from './containers/DBmakerForm';
 import AuthContainer from './containers/auths/AuthContainer';
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <Route path="/setting" component={SettingContainer} />
       <Route path="/maker" component={MakerLoading} />
       <Route path="/coment" component={ComentContainer} />
-      <Route path="/registerOnlylkjdrfs" component={DBmakerForm} />
+      {/* <Route path="/registerOnlylkjdrfs" component={DBmakerForm} /> */}
       {/* <Route path="/priceAPI" component={PriceAPI} />
       <Route path="/pricesave" component={PriceSave} /> */}
     </>
